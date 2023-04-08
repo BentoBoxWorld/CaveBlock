883b75020e91ab32bbe12707de74216ef7c500a9
BONNe <bonne@bonne.id.lv>
2023-04-08 19:12:44 +0300

    Update CaveBlockPladdon.java

--- src/main/java/world/bentobox/caveblock/CaveBlockPladdon.java
+++ src/main/java/world/bentobox/caveblock/CaveBlockPladdon.java
@@ -1,14 +1,10 @@
 package world.bentobox.caveblock;
 
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
 
-@Plugin(name = "CaveBlock", version = "1.0")
-@ApiVersion(ApiVersion.Target.v1_18)
 public class CaveBlockPladdon extends Pladdon {
 
     @Override
